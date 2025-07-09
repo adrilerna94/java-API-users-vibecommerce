51657438061b5f5ed891a32a274508e81427ea37
adrilerna94 <you@example.comgit config --global user.email you@example.com>
2025-07-09 22:08:03 +0200

    feat: ValidPassword interface annotation done

--- src/main/java/com/adriauson/vibecommerce/vibecommerce/validator/ValidPassword.java
+++ src/main/java/com/adriauson/vibecommerce/vibecommerce/validator/ValidPassword.java
@@ -1,4 +1,17 @@
 package com.adriauson.vibecommerce.vibecommerce.validator;
 
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.*;
+
+@Documented
+@Constraint(validatedBy = PasswordValidator.class)
+@Target({ ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
 public @interface ValidPassword {
+    String message() default "Password must contain at leat one uppercase, one lowercase, and be at least 5 characters long";
+    Class<?>[] groups() default {};
+    Class <? extends Payload>[] payload() default {};
 }
