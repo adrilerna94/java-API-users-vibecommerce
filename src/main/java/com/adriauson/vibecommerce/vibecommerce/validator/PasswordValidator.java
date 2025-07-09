9a5c64c5f749572b5fc479ec1be88729bab559ca
adrilerna94 <you@example.comgit config --global user.email you@example.com>
2025-07-09 22:19:08 +0200

    feat: :white_check_mark: PasswordValitor class done

--- src/main/java/com/adriauson/vibecommerce/vibecommerce/validator/PasswordValidator.java
+++ src/main/java/com/adriauson/vibecommerce/vibecommerce/validator/PasswordValidator.java
@@ -1,4 +1,16 @@
 package com.adriauson.vibecommerce.vibecommerce.validator;
 
-public class PasswordValidator {
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class PasswordValidator implements ConstraintValidator<ValidPassword, String> {
+    private static final String PASSWORD_REGEX = "^(?=.*[a-z])(?=.*[A-Z]).{5,}$";
+
+    @Override
+    public boolean isValid(String password, ConstraintValidatorContext context) {
+        if (password == null) {
+            return false;
+        }
+        return password.matches(PASSWORD_REGEX);
+    }
 }
